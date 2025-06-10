23807276c26211c6d59d5d2c830e1f7bd703b0d6
Meike Ahlers <meike.ahlers23@gmail.com>
2025-06-10 20:23:38 +1200

    font and bg fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased main-bg h-dvh`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased main-bg h-max`}
       >
         {children}
       </body>
