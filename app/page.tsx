58ee1a25b8a238cb569befbf0561a8b07da709e5
Meike Ahlers <meike.ahlers23@gmail.com>
2025-06-10 20:34:35 +1200

    font-fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,16 @@ import { Honk } from "next/font/google";
 const honk = Honk({
   subsets: ["latin"], // Essential for performance
   weight: ["400"], // Specify weights you actually use
-  variable: "--font-honk", // Optional, useful for Tailwind CSS
+  // variable: "--font-honk", // Optional, useful for Tailwind CSS
   display: "swap", // Recommended for optimal loading (prevents invisible text)
 });
 
 export default function Home() {
   return (
     <div className="m-5 flex flex-col justify-center">
-      <h1 className={`${honk.className} text-6xl text-center my-5`}>
+      <h1
+        className={`${honk.className} honk-text-fix text-6xl text-center my-5`}
+      >
         Schnuggis big birthday riddle 2025
       </h1>
       <Riddle />
